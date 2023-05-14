8b68222a06f1e85905d560401388cd48e1e367f7
Wilbert Josh Alfornon <114338772+wbert@users.noreply.github.com>
2023-05-14 17:16:26 +0800

    3rd commit

--- views/components/footer.php
+++ views/components/footer.php
@@ -6,7 +6,7 @@
     document.write(new Date().getFullYear());
   </script>
   , made with ❤️ by
-  <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+  </strong>AquaSense</strong>
 </div>
 
 </div>
